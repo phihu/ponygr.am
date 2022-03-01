2d56f0140d9f60eeab423944a6ef99938dce8df6
Philipp <philipp.hunstein@gmail.com>
2022-03-01 16:04:10 +0100

    work on base layout

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -64,33 +64,18 @@ class _PonygramPageState extends State<PonygramHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      body: Center(
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: Stack(
+        children:<Widget>[
+          Container(
+            constraints: BoxConstraints.expand(),
+            color:Colors.black,
+            child:
+              FittedBox(
+                  fit: BoxFit.fill,
+                  child: Image.asset('assets/testbg.jpg'),
+              ),
+          ),
+        ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
