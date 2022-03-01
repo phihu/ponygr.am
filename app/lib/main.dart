2116a5de9aac523ee093629e4777f63249b461f5
Philipp <philipp.hunstein@gmail.com>
2022-03-01 11:35:03 +0100

    wip

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -94,11 +94,6 @@ class _PonygramPageState extends State<PonygramHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
